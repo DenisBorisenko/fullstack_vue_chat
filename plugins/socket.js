282562454923dbf20bc99f5c0b20b73f402c52b8
Denis <denisborisenko982@mail.ru>
2019-11-07 17:25:04 +0300

    v3 prod dep

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function({store}){
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: 'http://localhost:3000',
+    connection: 'https://telegram3.herokuapp.com/',
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
